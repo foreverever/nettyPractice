b2999af8ba654fc4d6fcc17035e95da5820a9502
ajrwk384 <ajrwk384@naver.com>
2019-12-10 10:32:55 +0900

    netty - jpa 연동

---
+++ practice/practice/src/main/java/practice/configuration/SqlMapperConfig.java
@@ -0,0 +1,54 @@
+//package practice.configuration;
+//
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.mybatis.spring.SqlSessionFactoryBean;
+//import org.mybatis.spring.SqlSessionTemplate;
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//@MapperScan("practice")
+//@PropertySource(value = "classpath:/properties/sqlmap.properties")
+//@EnableTransactionManagement
+//public class SqlMapperConfig {
+//
+//    @Value("${mapper.location}")
+//    private Resource[] mapperLocation;
+//
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactory() throws Exception {
+//        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+//        sqlSessionFactory.setDataSource(dataSource());
+//        sqlSessionFactory.setMapperLocations(mapperLocation);
+//        return sqlSessionFactory.getObject();
+//    }
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        return new EmbeddedDatabaseBuilder()
+//                .setType(EmbeddedDatabaseType.H2)
+//                .addScript("/database/h2/schema.sql")
+//                .addScript("/database/h2/insert.sql")
+//                .build();
+//    }
+//
+//    @Bean
+//    public SqlSessionTemplate sqlSessionTemplate() throws Exception{
+//        return new SqlSessionTemplate(sqlSessionFactory());
+//    }
+//
+//    @Bean
+//    public DataSourceTransactionManager dataSourceTransactionManager(){
+//        return new DataSourceTransactionManager(dataSource());
+//    }
+//}
--- practice/practice/src/main/java/practice/handler/ServiceHandler.java
+++ practice/practice/src/main/java/practice/handler/ServiceHandler.java
@@ -1,6 +1,5 @@
 package practice.handler;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -9,9 +8,12 @@ import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import practice.domain.Message;
+import practice.domain.MessageRepository;
 
-import java.nio.charset.Charset;
+import java.util.List;
 
 
 /**
@@ -21,53 +23,46 @@ import java.nio.charset.Charset;
 @ChannelHandler.Sharable
 public class ServiceHandler extends ChannelInboundHandlerAdapter {
 
-    /**
-     * The Logger.
-     */
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    /**
-     * The Channels.
-     */
     private final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
-    /**
-     * Channel active.
-     *
-     * @param ctx the ctx
-     * @throws Exception the exception
-     */
-    //소켓채널이 최초 활성화 되었을 때(연결되면) 발생하는 이벤튼
+//    @Resource(name = "messageMapper")
+//    private MessageMapper messageMapper;
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    //소켓채널이 최초 활성화 되었을 때(연결되면) 발생하는 이벤트
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("time : " + System.currentTimeMillis());
+        System.out.println("channelActive called!!");
 //        channels.add(ctx.channel());
-        System.out.println("endActive!!");
     }
-
-    /**
-     * Channel read.
-     *
-     * @param ctx the ctx
-     * @param msg the msg
-     * @throws Exception the exception
-     */
 
     //메세지(데이터)가 들어올 때마다 호출된다
     //데이터 수신 이벤트 처리 메서드, 클라이언트로부터 데이터 수신이 이루어지는 경우 네티가 자동적으로 호출
     //데이터 로직 처리
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("msg : " + msg.toString());
-        ByteBuf byteBuf = (ByteBuf) msg;    //여기서 임의의 데이터를 생성?? tcp기반 서버가 보낸 순서대로 바이트를 수신할 수 있게 보장한다.
-        System.out.println("!!!!!!!!!!!!!!");
-        System.out.println("message : {} " + byteBuf.toString(Charset.defaultCharset()));
-        channels.writeAndFlush(msg);
+//        ByteBuf byteBuf = (ByteBuf) msg;    //여기서 임의의 데이터를 생성?? tcp기반 서버가 보낸 순서대로 바이트를 수신할 수 있게 보장한다.
+//        System.out.println("message : {} " + byteBuf.toString(Charset.defaultCharset()));
+        String stringMessage = (String) msg;
+        messageRepository.save(new Message(stringMessage));
+        System.out.println(stringMessage);
+
+        List<Message> messages = messageRepository.findAll();
+
+        for (Message message : messages) {
+            System.out.println(message.getId() + " : " + message.getContents());
+        }
+        channels.writeAndFlush(stringMessage);
+
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         super.channelReadComplete(ctx);
-        System.out.println("channelReadComplete call!!");
+        System.out.println("channelReadComplete called!!");
     }
 }
