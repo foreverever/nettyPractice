d7dbb7e77250fcb36a996d15057c23f408b098bf
ajrwk384 <ajrwk384@naver.com>
2019-12-11 22:13:54 +0900

    ver_04 complete (MessageDecoder 클래스 생성_서버측 수신 패킷 유효단위 분할 설정)

--- practice/practice/src/main/java/practice/handler/MessageHandler.java
+++ practice/practice/src/main/java/practice/handler/MessageHandler.java
@@ -1,5 +1,6 @@
 package practice.handler;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -14,18 +15,11 @@ import org.springframework.stereotype.Component;
 import practice.domain.Message;
 import practice.domain.MessageMapper;
 
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
+import java.net.InetSocketAddress;
 
-
-/**
- * The type Service handler.
- */
 @Component
 @ChannelHandler.Sharable
-public class ServiceHandler extends ChannelInboundHandlerAdapter {
+public class MessageHandler extends ChannelInboundHandlerAdapter {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -35,7 +29,7 @@ public class ServiceHandler extends ChannelInboundHandlerAdapter {
     private MessageMapper messageMapper;
 
     @Autowired
-    private RedisTemplate<String, Object> template;
+    private RedisTemplate<String, Object> redisTemplate;
 
     //소켓채널이 최초 활성화 되었을 때(연결되면) 발생하는 이벤트
     @Override
@@ -44,38 +38,57 @@ public class ServiceHandler extends ChannelInboundHandlerAdapter {
         channels.add(ctx.channel());
     }
 
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        logger.debug("channelInactive is called!!");
+        channels.remove(ctx.channel());
+    }
+
     //메세지(데이터)가 들어올 때마다 호출된다
     //데이터 수신 이벤트 처리 메서드, 클라이언트로부터 데이터 수신이 이루어지는 경우 네티가 자동적으로 호출
     //데이터 로직 처리
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-//        ByteBuf byteBuf = (ByteBuf) msg;    //여기서 임의의 데이터를 생성?? tcp기반 서버가 보낸 순서대로 바이트를 수신할 수 있게 보장한다.
+//        ByteBuf byteBuf = (ByteBuf) msg;    //tcp기반 서버가 보낸 순서대로 바이트를 수신할 수 있게 보장한다.
 //        System.out.println("message : {} " + byteBuf.toString(Charset.defaultCharset()));
-        String contents = (String) msg;
-        Message message = new Message(contents);
+        logger.debug("channelRead is called!!");
+
+        System.out.println(msg.toString());
+        String packet = (String) msg;
+        //todo : packet을 유효 데이터 단위로 쪼개야함 (IP Address/Contents/RequestTime)
+        Message message = new Message(packet);
         logger.debug("message : {}", message.getContents());
         logger.debug("messageSize : {}", message.getSize());
-        template.opsForHash().putAll("String.valueOf(message.getId())", message.getHashMapValue());
+        logger.debug("messageId : {}", message.getId());
 
-
+        //todo : 레디스 연결부는 서비스 레이어를 따로 만들어서 처리하는게 깔끔할 듯?
+//        redisTemplate.opsForHash().putAll(String.valueOf(message.getId()), message.getHashMapValue());
 //        messageMapper.save(new Message(stringMessage));
 //        List<Message> messages = messageMapper.findAll();
 //
 //        for (Message message : messages) {
 //            logger.debug("message info // id : {} , contents : {}", message.getId(), message.getContents());
 //        }
-        channels.writeAndFlush(contents);
+        String ipAddress = findIpAddress(ctx.channel());
+        logger.debug("Request IP Address : {}", ipAddress);
+
+        //메세지 전달
+        channels.writeAndFlush(message);
+    }
+
+    private String findIpAddress(Channel channel) {
+        InetSocketAddress socketAddress = (InetSocketAddress) channel.remoteAddress();
+        return socketAddress.getAddress().getHostAddress();
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         super.channelReadComplete(ctx);
-        System.out.println("channelReadComplete is called!!");
-//        ctx.close();
+        logger.debug("channelReadComplete is called!!");
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         logger.error(cause.getMessage(), cause);
         ctx.close();
     }
--- practice/practice/src/main/java/practice/netty/NettyServerInitializer.java
+++ practice/practice/src/main/java/practice/netty/NettyServerInitializer.java
@@ -1,32 +1,46 @@
 package practice.netty;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.*;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import practice.handler.ServiceHandler;
+import practice.handler.MessageHandler;
+
+import java.util.List;
 
 @Component
+@ChannelHandler.Sharable
 public class NettyServerInitializer extends ChannelInitializer<SocketChannel> {
 
     private static final StringDecoder DECODER = new StringDecoder();
     private static final StringEncoder ENCODER = new StringEncoder();
 
     @Autowired
-    private ServiceHandler serviceHandler;
+    private MessageHandler messageHandler;
 
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         ChannelPipeline pipeline = socketChannel.pipeline();
 
+        pipeline.addLast(new ByteToMessageDecoder() {
+            @Override
+            protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+                list.add(byteBuf.readBytes(byteBuf.readableBytes()));
+            }
+        });
         pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+        pipeline.addLast(new MessageDecoder());
         pipeline.addLast(DECODER);
         pipeline.addLast(ENCODER);
-        pipeline.addLast(serviceHandler);
+        pipeline.addLast(messageHandler);
     }
 }
