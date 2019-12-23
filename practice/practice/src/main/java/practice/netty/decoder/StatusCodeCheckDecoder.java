8b80a32b5f539c7d86032ecc7a917296db69f932
ajrwk384 <ajrwk384@naver.com>
2019-12-23 22:55:20 +0900

    ver_14 complete (DataRangeException(데이터 숫자 범위 벗어난 경우) 예외처리 추가)

--- practice/practice/src/main/java/practice/netty/decoder/StatusCodeCheckDecoder.java
+++ practice/practice/src/main/java/practice/netty/decoder/StatusCodeCheckDecoder.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import practice.domain.redis.RedisRepository;
 import practice.exception.DataContentException;
+import practice.exception.DataRangeException;
 import practice.exception.StatusCodeException;
 import practice.service.MessageService;
 import practice.support.StatusCode;
@@ -23,6 +24,7 @@ public class StatusCodeCheckDecoder extends MessageToMessageDecoder<String> {
     private static final int STATUS_CODE_FIELD = 4;
     private static final String WRONG_STATUS_CODE = "wrsc";
     private static final String WRONG_DATA = "wrdc";
+    private static final String OUT_OF_RANGE_DATA = "orda";
 
     @Override
     protected void decode(ChannelHandlerContext ctx, String packet, List<Object> list) throws Exception {
@@ -33,8 +35,14 @@ public class StatusCodeCheckDecoder extends MessageToMessageDecoder<String> {
         //데이터가 오는 경우
         if (statusCode == DATA) {
             String content = packet.substring(STATUS_CODE_FIELD, STATUS_CODE_FIELD + 4);
+            //데이터가 숫자가 아닌 경우
             if (isNotNumber(content)) {
                 exceptionCaught(ctx, new DataContentException(WRONG_DATA));    //wrong data content -> data를 해석할 수 없는 경우(문자 포함 한 경우)
+                return;
+            }
+            //데이터가 범위를 벗어 나는 경우(1번~9번 사이가 아닌 경우)
+            if (isNotValidRange(content)) {
+                exceptionCaught(ctx, new DataRangeException(OUT_OF_RANGE_DATA));
                 return;
             }
             logger.debug("contents information : {}", content);
@@ -48,5 +56,10 @@ public class StatusCodeCheckDecoder extends MessageToMessageDecoder<String> {
         else if (statusCode == FAKE) {
             list.add(packet);
         }
+    }
+
+    private boolean isNotValidRange(String content) {
+        int number = Integer.parseInt(content);
+        return number <= 0 || number >= 10;
     }
 }
